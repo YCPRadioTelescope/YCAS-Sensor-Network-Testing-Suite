176c8acdf4f52f4dbcfd301531e743269c352828
David Nelson <supermariokart98@gmail.com>
2021-02-14 11:06:19 -0500

    Added icon and updated form buttons to be taller

--- EmbeddedSystemsTest/EmbeddedSystemsTest/tcpClientServer.Designer.cs
+++ EmbeddedSystemsTest/EmbeddedSystemsTest/tcpClientServer.Designer.cs
@@ -29,6 +29,7 @@ namespace EmbeddedSystemsTest
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmTcpTest));
             this.btnStartListen = new System.Windows.Forms.Button();
             this.btnKillListen = new System.Windows.Forms.Button();
             this.txtListenIp = new System.Windows.Forms.TextBox();
@@ -54,9 +55,10 @@ namespace EmbeddedSystemsTest
             // 
             // btnStartListen
             // 
-            this.btnStartListen.Location = new System.Drawing.Point(15, 65);
+            this.btnStartListen.Location = new System.Drawing.Point(11, 53);
+            this.btnStartListen.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnStartListen.Name = "btnStartListen";
-            this.btnStartListen.Size = new System.Drawing.Size(139, 23);
+            this.btnStartListen.Size = new System.Drawing.Size(104, 21);
             this.btnStartListen.TabIndex = 2;
             this.btnStartListen.Text = "Start Listening";
             this.btnStartListen.UseVisualStyleBackColor = true;
@@ -65,9 +67,10 @@ namespace EmbeddedSystemsTest
             // btnKillListen
             // 
             this.btnKillListen.Enabled = false;
-            this.btnKillListen.Location = new System.Drawing.Point(160, 65);
+            this.btnKillListen.Location = new System.Drawing.Point(120, 53);
+            this.btnKillListen.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnKillListen.Name = "btnKillListen";
-            this.btnKillListen.Size = new System.Drawing.Size(139, 23);
+            this.btnKillListen.Size = new System.Drawing.Size(104, 21);
             this.btnKillListen.TabIndex = 9;
             this.btnKillListen.Text = "Stop Listening";
             this.btnKillListen.UseVisualStyleBackColor = true;
@@ -75,91 +78,102 @@ namespace EmbeddedSystemsTest
             // 
             // txtListenIp
             // 
-            this.txtListenIp.Location = new System.Drawing.Point(166, 6);
+            this.txtListenIp.Location = new System.Drawing.Point(124, 5);
+            this.txtListenIp.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtListenIp.Name = "txtListenIp";
-            this.txtListenIp.Size = new System.Drawing.Size(133, 22);
+            this.txtListenIp.Size = new System.Drawing.Size(101, 20);
             this.txtListenIp.TabIndex = 0;
             // 
             // lblListenIp
             // 
             this.lblListenIp.AutoSize = true;
-            this.lblListenIp.Location = new System.Drawing.Point(12, 9);
+            this.lblListenIp.Location = new System.Drawing.Point(9, 7);
+            this.lblListenIp.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblListenIp.Name = "lblListenIp";
-            this.lblListenIp.Size = new System.Drawing.Size(148, 17);
+            this.lblListenIp.Size = new System.Drawing.Size(111, 13);
             this.lblListenIp.TabIndex = 3;
             this.lblListenIp.Text = "IP address to listen to:";
             // 
             // lblListenRunning
             // 
             this.lblListenRunning.AutoSize = true;
-            this.lblListenRunning.Location = new System.Drawing.Point(18, 91);
+            this.lblListenRunning.Location = new System.Drawing.Point(14, 74);
+            this.lblListenRunning.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblListenRunning.Name = "lblListenRunning";
-            this.lblListenRunning.Size = new System.Drawing.Size(86, 17);
+            this.lblListenRunning.Size = new System.Drawing.Size(65, 13);
             this.lblListenRunning.TabIndex = 4;
             this.lblListenRunning.Text = "Not running.";
             // 
             // lblListenPort
             // 
             this.lblListenPort.AutoSize = true;
-            this.lblListenPort.Location = new System.Drawing.Point(12, 36);
+            this.lblListenPort.Location = new System.Drawing.Point(9, 29);
+            this.lblListenPort.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblListenPort.Name = "lblListenPort";
-            this.lblListenPort.Size = new System.Drawing.Size(107, 17);
+            this.lblListenPort.Size = new System.Drawing.Size(80, 13);
             this.lblListenPort.TabIndex = 5;
             this.lblListenPort.Text = "Port to listen to:";
             // 
             // txtListenPort
             // 
-            this.txtListenPort.Location = new System.Drawing.Point(166, 33);
+            this.txtListenPort.Location = new System.Drawing.Point(124, 27);
+            this.txtListenPort.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtListenPort.Name = "txtListenPort";
-            this.txtListenPort.Size = new System.Drawing.Size(133, 22);
+            this.txtListenPort.Size = new System.Drawing.Size(101, 20);
             this.txtListenPort.TabIndex = 1;
             // 
             // txtClientPort
             // 
-            this.txtClientPort.Location = new System.Drawing.Point(483, 33);
+            this.txtClientPort.Location = new System.Drawing.Point(362, 27);
+            this.txtClientPort.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtClientPort.Name = "txtClientPort";
-            this.txtClientPort.Size = new System.Drawing.Size(133, 22);
+            this.txtClientPort.Size = new System.Drawing.Size(101, 20);
             this.txtClientPort.TabIndex = 4;
             // 
             // lblClientPort
             // 
             this.lblClientPort.AutoSize = true;
-            this.lblClientPort.Location = new System.Drawing.Point(329, 36);
+            this.lblClientPort.Location = new System.Drawing.Point(247, 29);
+            this.lblClientPort.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblClientPort.Name = "lblClientPort";
-            this.lblClientPort.Size = new System.Drawing.Size(76, 17);
+            this.lblClientPort.Size = new System.Drawing.Size(57, 13);
             this.lblClientPort.TabIndex = 12;
             this.lblClientPort.Text = "Client port:";
             // 
             // lblData
             // 
             this.lblData.AutoSize = true;
-            this.lblData.Location = new System.Drawing.Point(329, 63);
+            this.lblData.Location = new System.Drawing.Point(247, 51);
+            this.lblData.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblData.Name = "lblData";
-            this.lblData.Size = new System.Drawing.Size(42, 17);
+            this.lblData.Size = new System.Drawing.Size(33, 13);
             this.lblData.TabIndex = 11;
             this.lblData.Text = "Data:";
             // 
             // lblClientIp
             // 
             this.lblClientIp.AutoSize = true;
-            this.lblClientIp.Location = new System.Drawing.Point(329, 9);
+            this.lblClientIp.Location = new System.Drawing.Point(247, 7);
+            this.lblClientIp.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblClientIp.Name = "lblClientIp";
-            this.lblClientIp.Size = new System.Drawing.Size(118, 17);
+            this.lblClientIp.Size = new System.Drawing.Size(89, 13);
             this.lblClientIp.TabIndex = 10;
             this.lblClientIp.Text = "Client IP address:";
             // 
             // txtClientIp
             // 
-            this.txtClientIp.Location = new System.Drawing.Point(483, 6);
+            this.txtClientIp.Location = new System.Drawing.Point(362, 5);
+            this.txtClientIp.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtClientIp.Name = "txtClientIp";
-            this.txtClientIp.Size = new System.Drawing.Size(133, 22);
+            this.txtClientIp.Size = new System.Drawing.Size(101, 20);
             this.txtClientIp.TabIndex = 3;
             // 
             // btnStartClient
             // 
-            this.btnStartClient.Location = new System.Drawing.Point(483, 88);
+            this.btnStartClient.Location = new System.Drawing.Point(362, 72);
+            this.btnStartClient.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnStartClient.Name = "btnStartClient";
-            this.btnStartClient.Size = new System.Drawing.Size(133, 23);
+            this.btnStartClient.Size = new System.Drawing.Size(100, 21);
             this.btnStartClient.TabIndex = 6;
             this.btnStartClient.Text = "Send Data";
             this.btnStartClient.UseVisualStyleBackColor = true;
@@ -168,62 +182,69 @@ namespace EmbeddedSystemsTest
             // lblListenConnected
             // 
             this.lblListenConnected.AutoSize = true;
-            this.lblListenConnected.Location = new System.Drawing.Point(163, 91);
+            this.lblListenConnected.Location = new System.Drawing.Point(122, 74);
+            this.lblListenConnected.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblListenConnected.Name = "lblListenConnected";
-            this.lblListenConnected.Size = new System.Drawing.Size(104, 17);
+            this.lblListenConnected.Size = new System.Drawing.Size(81, 13);
             this.lblListenConnected.TabIndex = 15;
             this.lblListenConnected.Text = "Not connected.";
             // 
             // txtClientData
             // 
-            this.txtClientData.Location = new System.Drawing.Point(483, 60);
+            this.txtClientData.Location = new System.Drawing.Point(362, 49);
+            this.txtClientData.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtClientData.Name = "txtClientData";
-            this.txtClientData.Size = new System.Drawing.Size(133, 22);
+            this.txtClientData.Size = new System.Drawing.Size(101, 20);
             this.txtClientData.TabIndex = 5;
             // 
             // lblResponse
             // 
             this.lblResponse.AutoSize = true;
-            this.lblResponse.Location = new System.Drawing.Point(329, 120);
+            this.lblResponse.Location = new System.Drawing.Point(247, 98);
+            this.lblResponse.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblResponse.Name = "lblResponse";
-            this.lblResponse.Size = new System.Drawing.Size(159, 17);
+            this.lblResponse.Size = new System.Drawing.Size(118, 13);
             this.lblResponse.TabIndex = 17;
             this.lblResponse.Text = "Responses from server:";
             // 
             // txtResponse
             // 
-            this.txtResponse.Location = new System.Drawing.Point(332, 140);
+            this.txtResponse.Location = new System.Drawing.Point(249, 114);
+            this.txtResponse.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtResponse.Multiline = true;
             this.txtResponse.Name = "txtResponse";
             this.txtResponse.ReadOnly = true;
             this.txtResponse.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtResponse.Size = new System.Drawing.Size(284, 120);
+            this.txtResponse.Size = new System.Drawing.Size(214, 98);
             this.txtResponse.TabIndex = 18;
             // 
             // txtReceived
             // 
-            this.txtReceived.Location = new System.Drawing.Point(21, 140);
+            this.txtReceived.Location = new System.Drawing.Point(16, 114);
+            this.txtReceived.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtReceived.Multiline = true;
             this.txtReceived.Name = "txtReceived";
             this.txtReceived.ReadOnly = true;
             this.txtReceived.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtReceived.Size = new System.Drawing.Size(284, 120);
+            this.txtReceived.Size = new System.Drawing.Size(214, 98);
             this.txtReceived.TabIndex = 20;
             // 
             // lblReceived
             // 
             this.lblReceived.AutoSize = true;
-            this.lblReceived.Location = new System.Drawing.Point(18, 120);
+            this.lblReceived.Location = new System.Drawing.Point(14, 98);
+            this.lblReceived.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblReceived.Name = "lblReceived";
-            this.lblReceived.Size = new System.Drawing.Size(140, 17);
+            this.lblReceived.Size = new System.Drawing.Size(107, 13);
             this.lblReceived.TabIndex = 19;
             this.lblReceived.Text = "Received from client:";
             // 
             // btnClear
             // 
-            this.btnClear.Location = new System.Drawing.Point(21, 265);
+            this.btnClear.Location = new System.Drawing.Point(16, 215);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnClear.Name = "btnClear";
-            this.btnClear.Size = new System.Drawing.Size(139, 23);
+            this.btnClear.Size = new System.Drawing.Size(104, 21);
             this.btnClear.TabIndex = 7;
             this.btnClear.Text = "Clear data";
             this.btnClear.UseVisualStyleBackColor = true;
@@ -231,9 +252,10 @@ namespace EmbeddedSystemsTest
             // 
             // btnClearAllResponses
             // 
-            this.btnClearAllResponses.Location = new System.Drawing.Point(332, 266);
+            this.btnClearAllResponses.Location = new System.Drawing.Point(249, 216);
+            this.btnClearAllResponses.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnClearAllResponses.Name = "btnClearAllResponses";
-            this.btnClearAllResponses.Size = new System.Drawing.Size(139, 23);
+            this.btnClearAllResponses.Size = new System.Drawing.Size(104, 21);
             this.btnClearAllResponses.TabIndex = 8;
             this.btnClearAllResponses.Text = "Clear responses";
             this.btnClearAllResponses.UseVisualStyleBackColor = true;
@@ -241,9 +263,9 @@ namespace EmbeddedSystemsTest
             // 
             // frmTcpTest
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(630, 300);
+            this.ClientSize = new System.Drawing.Size(472, 244);
             this.Controls.Add(this.btnClearAllResponses);
             this.Controls.Add(this.btnClear);
             this.Controls.Add(this.txtReceived);
@@ -266,6 +288,8 @@ namespace EmbeddedSystemsTest
             this.Controls.Add(this.btnKillListen);
             this.Controls.Add(this.btnStartListen);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.Name = "frmTcpTest";
             this.Text = "TCP Server/Client Test";
